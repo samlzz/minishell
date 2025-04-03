3e02e3aa723075575214ecfde8f98ee26433bb97
um4ss <sliziard@icloud.com>
2025-04-03 15:58:43 +0200

    test(parsing): add a print_err function that print err msg from the errcode or the errtok

--- src/main.c
+++ src/main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include "ast.h"
+#include <stdlib.h>
 
 
 void	print_ast_ascii(t_ast *node);
 
-void	print_err(int16_t err)
+static inline char	*token_type_str(t_tk_type type)
 {
-	if (err == PARSE_ERR_SQUOTE)
-		fprintf(stderr, "minishell: unexpected EOF while looking for matching `''\n");
-	if (err == PARSE_ERR_DQUOTE)
-		fprintf(stderr, "minishell: unexpected EOF while looking for matching `\"'\n");
-	else if (err == PARSE_ERR_EOF)
-		fprintf(stderr, "minishell: syntax error: unexpected end of file\n");
+	if (type == TK_WORD) return "WORD";
+	if (type == TK_PIPE) return "|";
+	if (type == TK_AND) return "&&";
+	if (type == TK_OR) return "||";
+	if (type == TK_REDIR_IN) return "<";
+	if (type == TK_REDIR_OUT) return ">";
+	if (type == TK_REDIR_APPEND) return ">>";
+	if (type == TK_HEREDOC) return "<<";
+	if (type == TK_EOF) return "EOF";
+	return "UNKNOWN";
+}
+void	print_err(int16_t errcode, t_token *errtok)
+{
+	char	*tk;
+
+	if (errcode == PARSE_ERR)
+		return (ft_putendl_fd("minishell: internal error occurs", 2));
+	else if (errcode == PARSE_ERR_SQUOTE)
+		return (ft_putendl_fd("minishell: unexpected EOF while looking for matching `\''", 2));
+	else if (errcode == PARSE_ERR_DQUOTE)
+		return (ft_putendl_fd("minishell: unexpected EOF while looking for matching `\"'", 2));
+	if (!errtok && errcode == PARSE_OK)
+		tk = "newline";
+	else
+		tk = errtok->value;
+	if (!tk)
+		tk = token_type_str(errtok->type);
+	fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", tk);
+	token_clear(errtok);
 }
 
 
@@ -21,20 +45,22 @@ int	main(void)
 	char	input[1024];
 	t_ast	*ast;
 	int16_t	err;
+	t_token	*errtok;
 
 	while (1)
 	{
+		ast = NULL;
 		printf("miniparse> ");
 		if (!fgets(input, sizeof(input), stdin))
 			break;
-
 		// Remove trailing newline
 		input[strcspn(input, "\n")] = '\0';
 
-		err = new_ast((const char *)input, &ast);
+		ast = new_ast((const char *)input, &err, &errtok);
 		if (!ast)
 		{
-			fprintf(stderr, "Erreur : échec de la création de l'ast. %d\n", err);
+			ft_putendl_fd("", 2);
+			print_err(err, errtok);
 			continue;
 		}
 		print_ast_ascii(ast);
