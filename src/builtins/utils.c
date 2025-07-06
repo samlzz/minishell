fb50ae0d0c1ac2283873fbc98ace11e1e17221b8
Mateo LE FLEM <mle-flem@student.42.fr>
2025-07-06 09:57:35 +0200

    refactor(exec): cleanup builtin invocation logic

--- src/builtins/utils.c
+++ src/builtins/utils.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 07:38:14 by mle-flem          #+#    #+#             */
-/*   Updated: 2025/07/01 17:44:59 by mle-flem         ###   ########.fr       */
+/*   Updated: 2025/07/06 09:38:25 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,25 @@ bool	get_builtin_func(char *cmd, t_builtin_func *func)
 		if (!ft_strcmp(names[i], cmd))
 			*func = funcs[i];
 	return (!!*func);
+}
+
+uint8_t	is_builtin(t_sh_ctx *ctx, t_ast *root, t_ast *node)
+{
+	t_token			*errtok;
+	t_builtin_func	func;
+
+	if (!node)
+		return (0);
+	if (node->type == ND_REDIR)
+		return (is_builtin(ctx, root, node->u_data.rd.child));
+	else if (node->type == ND_CMD)
+	{
+		errtok = NULL;
+		if (expand_node(ctx, node, &errtok))
+			return (err_print_expand(errtok), context_free(ctx), ast_free(root),
+				2);
+		else if (get_builtin_func(node->u_data.cmd.args->expanded, &func))
+			return (1);
+	}
+	return (0);
 }
