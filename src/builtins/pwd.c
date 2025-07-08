d30587710adbf0b025b753d7f9d0429df170d180
Mateo LE FLEM <mle-flem@student.42.fr>
2025-07-08 16:33:00 +0200

    feat(builtins): implement pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,22 +6,52 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 05:28:30 by mle-flem          #+#    #+#             */
-/*   Updated: 2025/07/06 05:26:08 by mle-flem         ###   ########.fr       */
+/*   Updated: 2025/07/08 06:42:42 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static inline bool	_check_invalid_opt(int32_t ac, char **av)
+{
+	int32_t	i;
+	char	erropt;
+	char	errstr[36];
+
+	erropt = 0;
+	i = 0;
+	while (++i < ac && ft_strcmp(av[i], "--"))
+	{
+		if (av[i][0] == '-' && av[i][1] != '\0')
+		{
+			erropt = av[i][1];
+			break ;
+		}
+	}
+	if (erropt)
+	{
+		errstr[0] = 0;
+		ft_strlcat(errstr, "minishell: pwd: -?: invalid option\n", 36);
+		errstr[17] = erropt;
+		ft_putstr_fd(errstr, STDERR_FILENO);
+	}
+	return (!!erropt);
+}
 
 int32_t	main_pwd(int32_t ac, char **av, t_sh_ctx *ctx)
 {
-	int32_t	i;
+	char	*pwd;
 
 	(void) ctx;
-	printf("Executing pwd:\n");
-	i = -1;
-	while (++i < ac)
-		printf("%d:\t%s\n", i, av[i]);
-	printf("%d:\t%s\n", i, av[i]);
+	if (_check_invalid_opt(ac, av))
+		return (2);
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		return (ft_putstr_fd("minishell: malloc", STDERR_FILENO), 1);
+	ft_putstr_fd(pwd, STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+	free(pwd);
 	return (0);
 }
