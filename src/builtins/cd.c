2f88c566befe2232028fa875a2fed2c8841197df
Mateo LE FLEM <mle-flem@student.42.fr>
2025-07-14 06:49:04 +0200

    fix: correctly handle empty dir path

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 05:28:30 by mle-flem          #+#    #+#             */
-/*   Updated: 2025/07/13 10:39:35 by mle-flem         ###   ########.fr       */
+/*   Updated: 2025/07/14 06:07:08 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ static inline bool	_get_dest_dir(char **dst, int32_t ac, char **av,
 	return (true);
 }
 
-static inline int32_t	_print_chdir_err(char *dir, char *dir_f, char *old_cwd)
+static inline int32_t	_print_chdir_err(char *dir, char *old_cwd)
 {
 	char	*err;
 	int32_t	errno_;
@@ -73,18 +73,25 @@ static inline int32_t	_print_chdir_err(char *dir, char *dir_f, char *old_cwd)
 	errno_ = errno;
 	err = ft_calloc(16 + ft_strlen(dir), sizeof(char));
 	if (!err)
-		return (perror("minishell: malloc"), free(dir_f), free(old_cwd), 1);
+		return (perror("minishell: malloc"), free(old_cwd), 1);
 	ft_strlcat(err, "minishell: cd: ", 16 + ft_strlen(dir));
 	ft_strlcat(err, dir, 16 + ft_strlen(dir));
 	errno = errno_;
-	return (perror(err), free(err), free(dir_f), free(old_cwd), 1);
+	return (perror(err), free(err), free(old_cwd), 1);
 }
 
-static inline int32_t	_print_cwd(bool should_print, char *cwd)
+static inline int32_t	_print_and_update_pwd(bool should_print, t_sh_ctx *ctx,
+												char *old_cwd)
 {
+	char	*new_cwd;
+
+	new_cwd = getcwd(NULL, 0);
+	if (!new_cwd || env_set(&ctx->env, "OLDPWD", old_cwd)
+		|| env_set(&ctx->env, "PWD", new_cwd))
+		return (perror("minishell: malloc"), 1);
 	if (should_print)
 	{
-		ft_putstr_fd(cwd, STDOUT_FILENO);
+		ft_putstr_fd(new_cwd, STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	}
 	return (0);
@@ -93,9 +100,7 @@ static inline int32_t	_print_cwd(bool should_print, char *cwd)
 int32_t	main_cd(int32_t ac, char **av, t_sh_ctx *ctx)
 {
 	char	*dir;
-	char	*dir_f;
-	char	*old_cwd;
-	char	*new_cwd;
+	char	*old;
 
 	if (_check_invalid_opt(&ac, &av))
 		return (2);
@@ -104,16 +109,12 @@ int32_t	main_cd(int32_t ac, char **av, t_sh_ctx *ctx)
 				STDERR_FILENO), 1);
 	if (!_get_dest_dir(&dir, ac, av, ctx))
 		return (1);
-	dir_f = (char *)((uintptr_t)dir * (!ac || !ft_strcmp(av[0], "-")));
-	old_cwd = getcwd(NULL, 0);
-	if (!old_cwd)
-		return (perror("minishell: malloc"), free(dir_f), 1);
+	old = getcwd(NULL, 0);
+	if (!old)
+		return (perror("minishell: malloc"), 1);
+	if (!dir[0])
+		return (_print_and_update_pwd(false, ctx, old));
 	if (chdir(dir))
-		return (_print_chdir_err(dir, dir_f, old_cwd));
-	env_set(&ctx->env, "OLDPWD", old_cwd);
-	new_cwd = getcwd(NULL, 0);
-	if (!new_cwd)
-		return (perror("minishell: malloc"), free(dir_f), 1);
-	env_set(&ctx->env, "PWD", new_cwd);
-	return (free(dir_f), _print_cwd(ac > 0 && !ft_strcmp(av[0], "-"), new_cwd));
+		return (_print_chdir_err(dir, old));
+	return (_print_and_update_pwd(ac > 0 && !ft_strcmp(av[0], "-"), ctx, old));
 }
