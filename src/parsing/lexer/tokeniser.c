c2684c0af237033dce52e9c739ab702f289b874e
um4ss <sliziard@icloud.com>
2025-07-24 19:03:02 +0200

    fix(parsing): handle pipe in tokenise in mandatory part

--- src/parsing/lexer/tokeniser.c
+++ src/parsing/lexer/tokeniser.c
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 14:17:47 by sliziard          #+#    #+#             */
-/*   Updated: 2025/07/24 11:13:54 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/07/24 15:57:31 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,6 +105,8 @@ static int32_t	_fill_token(const char *input, t_token *curr)
 {
 	if (!*input)
 		return (curr->type = TK_EOF, 0);
+	else if (*input == '|')
+		curr->type = TK_PIPE;
 	else if (input[0] == '>')
 		curr->type = (input[1] == '>') * TK_REDIR_APPEND
 			+ !(input[1] == '>') * TK_REDIR_OUT;
