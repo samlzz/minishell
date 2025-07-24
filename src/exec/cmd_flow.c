96f1f0315b1860ece939b6b86b5b3647e3ae355f
Mateo LE FLEM <mle-flem@student.42.fr>
2025-07-24 03:08:03 +0200

    refactor(exec): remove exec_flow_builtin from decl

--- src/exec/cmd_flow.c
+++ src/exec/cmd_flow.c
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 18:54:02 by mle-flem          #+#    #+#             */
-/*   Updated: 2025/07/23 22:06:37 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/07/24 03:01:18 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include "builtins/builtins.h"
 #include "exec/exec.h"
 
 static void	_dup_err(int32_t oldfd, int32_t newfd)
@@ -119,7 +118,7 @@ static void	_exec_flow_cmd_cmd(t_sh_ctx *ctx, t_ast *node, int32_t fds[2])
 		ft_splitfree(envp, 0), context_free(ctx), exit(126));
 }
 
-void	exec_flow_builtin(t_sh_ctx *ctx, t_ast *node, int32_t fds[2])
+static void	_exec_flow_cmd_builtin(t_sh_ctx *ctx, t_ast *node, int32_t fds[2])
 {
 	char			**av;
 	int32_t			old_fds[2];
@@ -154,7 +153,7 @@ void	exec_flow_cmd(t_sh_ctx *ctx, t_ast *node, int32_t fds[2])
 			return (err_print_expand(errtok), _close_all_fds(fds),
 				context_free(ctx), exit(1));
 		else if (node->u_data.cmd.bi)
-			exec_flow_builtin(ctx, node, fds);
+			_exec_flow_cmd_builtin(ctx, node, fds);
 		else
 			_exec_flow_cmd_cmd(ctx, node, fds);
 	}
--- src/exec/exec.h
+++ src/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 18:19:12 by mle-flem          #+#    #+#             */
-/*   Updated: 2025/07/24 01:07:41 by mle-flem         ###   ########.fr       */
+/*   Updated: 2025/07/24 01:54:36 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define EXEC_H
 
 # include "ast/ast.h"
-# include "builtins/builtins.h"
 
 uint8_t	exec_wrapper(t_sh_ctx *ctx, t_ast *node);
 
@@ -22,7 +21,6 @@ uint8_t	exec_flow_exec(t_sh_ctx *ctx, t_ast *node, int32_t fds[2]);
 void	exec_flow_pipe(t_sh_ctx *ctx, t_ast *node, int32_t fds[3]);
 void	exec_flow_cmd(t_sh_ctx *ctx, t_ast *node, int32_t fds[2]);
 void	exec_flow_redir(t_sh_ctx *ctx, t_ast *node, int32_t fds[2]);
-void	exec_flow_builtin(t_sh_ctx *ctx, t_ast *node, int32_t fds[2]);
 
 char	*exec_get_cmd_path(char **av, t_sh_ctx *ctx);
 size_t	exec_wait_get_count(t_ast *node, bool is_hd);
